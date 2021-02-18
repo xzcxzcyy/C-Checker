8db24a7441abf0eeb3f03bf86ad835d8cb540a49
Banson Xu <xzcxzcyy@126.com>
2021-02-18 22:52:31 +0800

    feat: Token.TokenType

--- lexer.hpp
+++ lexer.hpp
@@ -14,5 +14,5 @@ public:
     ~Lexer() {
         inputStream.close();
     }
-    
+
 };
--- token.hpp
+++ token.hpp
@@ -2,12 +2,55 @@
 
 class Token {
 public:
-    enum TokenType { IDENTIFIER, NUMBER, OPEN_PARAMETER, CLOSE_PARAM, CLOSE_PARAM };
+    enum TokenType {
+        VOID,
+        INT,
+        CHAR,
+        SHORT,
+        LONG,
+        DOUBLE,
+        FLOAT,
+        CONST,
+        WHILE,
+        FOR,
+        CONTINUE,
+        BREAK,
+        IF,
+        ELSE,
+        IDENTIFIER,
+        RETURN,
+        CONST_INT,
+        CONST_LONG,
+        CONST_FLOAT,
+        CONST_CHAR,
+        OPEN_PAREN,
+        CLOSE_PAREN,
+        OPEN_BRACKET,
+        CLOSE_BRACKET,
+        OPEN_BRACE,
+        CLOSE_BRACE,
+        PLUS,
+        MINUS,
+        TIMES,
+        DIVIDE,
+        GREATER,
+        GEQUAL,
+        LESS,
+        LEQUAL,
+        EQUAL,
+        NOT_EQUAL,
+        LOGICAL_AND,
+        LOGICAL_OR,
+        SEMICOL,
+        COMMA,
+        WHITESPACE,
+        ASSIGN,
+    };
     TokenType type;
     std::string name;
 
-    Token(std::string name) {
+    Token(std::string name, TokenType type) {
         this->name = name;
-        // TODO: handle TokenType
+        this->type = type;
     }
 };
