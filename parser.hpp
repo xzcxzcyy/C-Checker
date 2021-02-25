2bf199162326451252f55649373c23ed6805f1ad
Banson Xu <xzcxzcyy@126.com>
2021-02-25 22:28:05 +0800

    feat: Parser::parser.hpp

--- parser.hpp
+++ parser.hpp
@@ -1,3 +1,17 @@
-class Parser {
+#ifndef PARSER_HPP
+#define PARSER_HPP
 
+#include <vector>
+#include "token.hpp"
+
+class Parser {
+private:
+    std::vector<Token> tokens;
+    std::vector<Token>::iterator tokensIt;
+
+public:
+    Parser(std::vector<Token> ts) : tokens(ts) {
+        tokensIt = tokens.begin();
+    }
 };
+#endif
